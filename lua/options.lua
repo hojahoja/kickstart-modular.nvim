0c782c358ec4aa87490dfe78621c88c6b8725989
hojahoja <5722145+hojahoja@users.noreply.github.com>
2024-10-26 20:13:09 +0300

    added vscode specific setup

--- lua/options.lua
+++ lua/options.lua
@@ -3,22 +3,31 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Conditional keymaps depending on whether nvim is running inside vscode.
+if not vim.g.vscode then
+  -- Make line numbers default
+  vim.opt.number = true
+  -- You can also add relative line numbers, to help with jumping.
+  --  Experiment for yourself to see if you like it!
+  vim.opt.relativenumber = true
+  -- Don't show the mode, since it's already in the status line
+  vim.opt.showmode = false
+  -- Save undo history
+  vim.opt.undofile = true
+  -- Enable break indent
+  vim.opt.breakindent = true
+  -- Minimal number of screen lines to keep above and below the cursor.
+  vim.opt.scrolloff = 10
+end
+
 -- CUSTOM options start
 -- Langmap Finnish keyboard layout (No dead keys) to have English
 -- special character layout in normal mode
 vim.opt.langmap = '§½\\"¤&/()=;`~@$^&*(),+?´`;-_=+,åÅ¨^;[{]},öÖäÄ*;\\;:\'\\"\\|,\\;:-_;<>/?'
 -- CUSTOM options end
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
-vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
@@ -27,12 +36,6 @@ vim.opt.showmode = false
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
-
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
@@ -62,13 +65,5 @@ vim.opt.inccommand = 'split'
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-vim.opt.confirm = true
 
 -- vim: ts=2 sts=2 sw=2 et
