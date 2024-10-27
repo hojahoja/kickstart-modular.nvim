5adbf96ba7b41da373ac8e765d2d3f279469c376
hojahoja <5722145+hojahoja@users.noreply.github.com>
2024-10-27 19:42:54 +0200

    Refactor for conditional plugin load to prepare for VScode

--- init.lua
+++ init.lua
@@ -7,23 +7,17 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
-
 -- [[ Setting options ]]
 require 'options'
 
 -- [[ Basic Keymaps ]]
 require 'keymaps'
 
--- Checks if nvim is started inside VScode and picks config files accordingly
-if vim.g.vscode then
-  -- [[ Use VScode specific setups and plugins ]]
-  require 'vscode-lazy-plugins'
-else
-  -- [[ Configure and install plugins ]]
-  require 'lazy-plugins'
-end
+-- [[ Install `lazy.nvim` plugin manager ]]
+require 'lazy-bootstrap'
+
+-- [[ Configure and install plugins ]]
+require 'lazy-plugins'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
