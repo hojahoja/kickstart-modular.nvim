11f9bc30b805346de3e7c82eb472ffb676545158
hojahoja <5722145+hojahoja@users.noreply.github.com>
2024-12-03 21:49:26 +0200

    LSP config changes and mini indent plugin

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -211,7 +211,19 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
+
+        -- CUSTOM python LSP
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              analysis = {
+                diagnosticSeverityOverrides = {
+                  reportImplicitOverride = false,
+                },
+              },
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
